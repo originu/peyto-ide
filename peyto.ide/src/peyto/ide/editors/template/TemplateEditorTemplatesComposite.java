36c66be80e873a9587cca557da8a6fc42afe4658
originu <originu@gmail.com>
2022-12-23 02:05:12 +0900

    template 생성, 삭제 기능 추가

--- peyto.ide/src/peyto/ide/editors/template/TemplateEditorTemplatesComposite.java
+++ peyto.ide/src/peyto/ide/editors/template/TemplateEditorTemplatesComposite.java
@@ -2,7 +2,9 @@ package peyto.ide.editors.template;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
@@ -13,6 +15,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -41,6 +45,8 @@ public class TemplateEditorTemplatesComposite extends Composite {
 	private Text categoryNameText;
 	private ComboViewer revisionComboViewer;
 	private Text revisionNameText;
+	private Button addButton;
+	private Button deleteButton;
 	private StyledText contentStyledText;
 	
 	private TemplateCategoryDto templateCategoryDto;
@@ -52,7 +58,7 @@ public class TemplateEditorTemplatesComposite extends Composite {
 	 */
 	public TemplateEditorTemplatesComposite(Composite parent, int style) {
 		super(parent, style);
-		GridLayout gridLayout = new GridLayout(5, false);
+		GridLayout gridLayout = new GridLayout(6, false);
 		setLayout(gridLayout);
 		
 		Label categoryCodeLabel = new Label(this, SWT.NONE);
@@ -64,6 +70,7 @@ public class TemplateEditorTemplatesComposite extends Composite {
 		GridData gd_categoryCodeText = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
 		gd_categoryCodeText.widthHint = 250;
 		categoryCodeText.setLayoutData(gd_categoryCodeText);
+		new Label(this, SWT.NONE);
 		
 		Label categoryNameLabel = new Label(this, SWT.NONE);
 		categoryNameLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
@@ -102,10 +109,14 @@ public class TemplateEditorTemplatesComposite extends Composite {
 		});
 		Combo revisionCombo = revisionComboViewer.getCombo();
 		GridData gd_revisionCombo = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
-		gd_revisionCombo.widthHint = 100;
+		gd_revisionCombo.widthHint = 120;
 		revisionCombo.setLayoutData(gd_revisionCombo);
+		new Label(this, SWT.NONE);
 		
 		Button refreshButton = new Button(this, SWT.NONE);
+		GridData gd_refreshButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_refreshButton.widthHint = 80;
+		refreshButton.setLayoutData(gd_refreshButton);
 		refreshButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
@@ -125,10 +136,84 @@ public class TemplateEditorTemplatesComposite extends Composite {
 		GridData gd_revisionNameText = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		gd_revisionNameText.widthHint = 250;
 		revisionNameText.setLayoutData(gd_revisionNameText);
+		new Label(this, SWT.NONE);
+		
+		Button editableCheckButton = new Button(this, SWT.CHECK);
+		editableCheckButton.setText("Editable");
+		editableCheckButton.addSelectionListener(new SelectionAdapter( ) {
+		    public void widgetSelected(SelectionEvent e) {
+		    	if(templateCategoryDto != null) {
+		    		setEditableMode(editableCheckButton.getSelection());
+		    	}
+		    }
+		});
+		
+		addButton = new Button(this, SWT.NONE);
+		addButton.setEnabled(editableCheckButton.getSelection());
+		addButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				TemplateDto dto = new TemplateDto();
+				dto.setCategoryCode(templateCategoryDto.getCategoryCode());
+				dto.setRevisionName(revisionNameText.getText());
+				dto.setContent(contentStyledText.getText());
+				String resourcePath = String.format("/api/template");
+				httpService.post(resourcePath, dto, new ResponseHandler() {
+					@Override
+					public void completed(SimpleHttpResponse response) {
+						setEditableMode(false);
+						editableCheckButton.setSelection(false);
+						deleteButton.setSelection(true);
+						updateTemplatesByCategoryCode(templateCategoryDto);
+					}
+				});
+			}
+		});
+		GridData gd_addButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_addButton.widthHint = 80;
+		addButton.setLayoutData(gd_addButton);
+		addButton.setText("Add");
+		
+		deleteButton = new Button(this, SWT.NONE);
+		deleteButton.setEnabled(!editableCheckButton.getSelection());
+		deleteButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				IStructuredSelection selection = (IStructuredSelection) revisionComboViewer.getSelection();
+				TemplateDto dto = (TemplateDto) selection.getFirstElement();
+				boolean result = MessageDialog.openConfirm(
+						getShell(),
+						"Warning",
+						String.format(
+								"Are you sure to delete a template?\n- RevisionName: %s\n- Revision: %s",
+								dto.getRevisionName(),
+								dto.getRevision()
+								)
+						);
+				if (result) {
+					String resourcePath = String.format("/api/template/%d", dto.getId() );
+					httpService.delete(resourcePath, dto, new ResponseHandler() {
+						@Override
+						public void completed(SimpleHttpResponse response) {
+							setEditableMode(false);
+							editableCheckButton.setSelection(false);
+							updateTemplatesByCategoryCode(templateCategoryDto);
+						}
+					});
+		        	updateTemplatesByCategoryCode(templateCategoryDto);
+				}
+			}
+		});
+		GridData gd_deleteButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_deleteButton.widthHint = 80;
+		deleteButton.setLayoutData(gd_deleteButton);
+		deleteButton.setText("Delete");
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
 		
 		contentStyledText = new StyledText(this, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
 		contentStyledText.setEditable(false);
-		GridData gd_contentStyledText = new GridData(SWT.FILL, SWT.FILL, true, true, 5, 1);
+		GridData gd_contentStyledText = new GridData(SWT.FILL, SWT.FILL, true, true, 6, 1);
 		gd_contentStyledText.widthHint = 698;
 		contentStyledText.setLayoutData(gd_contentStyledText);
 
@@ -177,7 +262,7 @@ public class TemplateEditorTemplatesComposite extends Composite {
 					e.printStackTrace();
 				}
 			}
-		});			
+		});
 	}
 	
 	private void updateContent(long id) {
@@ -187,13 +272,20 @@ public class TemplateEditorTemplatesComposite extends Composite {
 			public void completed(SimpleHttpResponse response) {
 				try {
 					ResData<TemplateDto> data = JsonUtil.MAPPER.readValue(response.getBodyText(), new TypeReference<ResData<TemplateDto>>() {});
-					revisionNameText.setText(data.getBody().getRevisionName());
+					revisionNameText.setText(StringUtils.defaultIfEmpty(data.getBody().getRevisionName(), ""));
 					contentStyledText.setText(data.getBody().getContent());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});			
+	}
+	
+	private void setEditableMode(boolean enabled) {
+		revisionNameText.setEditable(enabled);
+		addButton.setEnabled(enabled);
+		deleteButton.setSelection(!enabled);
+		contentStyledText.setEditable(enabled);
 	}
 
 }
